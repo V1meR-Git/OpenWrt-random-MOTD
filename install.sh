5f7cbb9769e463d43165037a10b9316098362f17
V1meR-Git <mihalevmihail0@gmail.com>
2026-07-30 17:02:17 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,32 @@
 #!/bin/sh
+echo "Установка Git..."
+apk update && apk add git
+
+echo "Установлен, клонирование репозитория..."
+git clone https://github.com/V1meR-Git/OpenWrt-random-MOTD.git /root/OpenWrt-random-MOTD
+mkdir -p /root/banners
+
+echo "Копирование файлов..."
+cp /root/OpenWrt-random-MOTD/etc/profile.d/apk-cheatsheet.sh /etc/profile.d/apk-cheatsheet.sh
+cp /root/OpenWrt-random-MOTD/root/banners/* /root/banners/
+
+echo "Выдача прав..."
+chmod +x /etc/profile.d/apk-cheatsheet.sh
+chmod 644 /root/banners/*
+
+printf "Удалить git и git-http ? [y/N]: "
+read answer < /dev/tty
+case "$answer" in
+    [Yy]|[Yy][Ee][Ss])
+        apk del git
+        echo "Удалено."
+        ;;
+    *)
+        echo "Без изменений."
+        ;;
+esac
+
+echo "Очистка хвостов..."
+rm -rf /root/OpenWrt-random-MOTD
+
+echo "Готово, нужно переподключиться по ssh"
